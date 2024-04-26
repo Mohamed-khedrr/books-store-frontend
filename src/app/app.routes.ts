7e9d889ab6d4d75f87cc979a4255b4344430b9bf
Mohamed Khedr <khedr66664@gmail.com>
2024-04-27 02:39:26 +0300

    add order component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AllBooksComponent } from './all-books/all-books.component';
 import { CartComponent } from './cart/cart.component';
+import { OrderFormComponent } from './order-form/order-form.component';
 
 export const routes: Routes = [
   { path: '', component: AllBooksComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'order', component: OrderFormComponent },
 ];
